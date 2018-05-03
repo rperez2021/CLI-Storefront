35d5992bc23afd874fa603aa59dbb0616e34a48d
Roberto Perez <34112620+rperez2021@users.noreply.github.com>
2018-05-02 18:54:44 -0700

    Update bamazonCustomer.js

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,7 +7,7 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'root',
+    password: '',
     database: 'bamazon_db'
 });
 
